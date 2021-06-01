ce26b1ddab9ce5d17f57b7481797b40d3e3b2a0b
albertkun <albertk@gmx.com>
2021-06-01 06:39:47 -0700

    Release: `Chart.js`

---
+++ js/init.js
@@ -0,0 +1,129 @@
+const map = L.map('map').setView([34.0709, -118.444], 5);
+
+const url = "https://spreadsheets.google.com/feeds/list/1upD99bKWIO68jL8MKWV67KE-_H_TVn2bCwqyQkqNsBw/oxw5dh3/public/values?alt=json"
+
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+
+Esri_WorldGrayCanvas.addTo(map)
+
+fetch(url)
+	.then(response => {
+		return response.json();
+		})
+    .then(data =>{
+                formatData(data)
+        }
+)
+
+let speakFluentEnglish = L.featureGroup();
+let speakOtherLanguage = L.featureGroup();
+
+// add variables for keeping track of the count for the charts
+let countEnglish = 0;
+let countNonEnglish = 0;
+
+
+let exampleOptions = {
+    radius: 4,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+}
+
+function addMarker(data){
+    if(data.doyouspeakenglishfluently == "Yes"){
+        exampleOptions.fillColor = "green"
+        speakFluentEnglish.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak English fluently</h2>`))
+        createButtons(data.lat,data.lng,data.location)
+        // add to the running total
+        countEnglish += 1;
+        }
+    else{
+        exampleOptions.fillColor = "red"
+        speakOtherLanguage.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak other languages</h2>`))
+        createButtons(data.lat,data.lng,data.location)
+        // add to the running total of non-English 
+        countNonEnglish +=1;
+    }
+    return data.timestamp
+}
+
+function createButtons(lat,lng,title){
+    const newButton = document.createElement("button");
+    newButton.id = "button"+title;
+    newButton.innerHTML = title;
+    newButton.setAttribute("lat",lat); 
+    newButton.setAttribute("lng",lng);
+    newButton.addEventListener('click', function(){
+        map.flyTo([lat,lng]);
+    })
+    // removed buttons
+    // const spaceForButtons = document.getElementById('contents')
+    // spaceForButtons.appendChild(newButton);
+}
+
+function formatData(theData){
+        const formattedData = []
+        const rows = theData.feed.entry
+        for(const row of rows) {
+          const formattedRow = {}
+          for(const key in row) {
+            if(key.startsWith("gsx$")) {
+                  formattedRow[key.replace("gsx$", "")] = row[key].$t
+            }
+          }
+          formattedData.push(formattedRow)
+        }
+        console.log(formattedData)
+        formattedData.forEach(addMarker)
+        speakFluentEnglish.addTo(map)
+        console.log('speakFluentEnglish')
+        console.log(speakFluentEnglish)
+        speakOtherLanguage.addTo(map)
+        let allLayers = L.featureGroup([speakFluentEnglish,speakOtherLanguage]);
+        map.fitBounds(allLayers.getBounds());
+        addChart() // call the addChart function after the data is added
+}
+
+let layers = {
+	"Speaks English": speakFluentEnglish,
+	"Speaks Other Languages": speakOtherLanguage
+}
+
+//function to add chart after the data gets created
+
+function addChart(){
+    // create the new chart here, target the id in the html called "chart"
+    new Chart(document.getElementById("chart"), {
+        type: 'pie', //can change to 'bar','line' chart or others
+        data: {
+            // labels for data here
+        labels: ["Speak English Fluently","Speaks Other Language"],
+        datasets: [
+            {
+            label: "Count",
+            backgroundColor: ["green", "red"],
+            data: [countEnglish,countNonEnglish]
+            }
+        ]
+        },
+        options: {
+            responsive: true, //turn on responsive mode changes with page size
+            maintainAspectRatio: false, // if `true` causes weird layout issues
+            legend: { display: true },
+            title: {
+                display: true,
+                text: 'Survey Respondants'
+            }
+        }
+    });
+}
+
+
+
+L.control.layers(null,layers).addTo(map)
