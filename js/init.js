2a22d5d1542094612ccab9f36528a0f28ae53b83
albertkun <albertk@gmx.com>
2022-05-30 06:27:08 -0700

    release: v2.0 feat: papaparse added

--- js/init.js
+++ js/init.js
@@ -1,98 +1,103 @@
-const map = L.map('map').setView([34.0709, -118.444], 5);
+// declare variables
+let mapOptions = {'center': [34.0709,-118.444],'zoom':5};
 
-const url = "https://spreadsheets.google.com/feeds/list/1upD99bKWIO68jL8MKWV67KE-_H_TVn2bCwqyQkqNsBw/oxw5dh3/public/values?alt=json"
+let englishFirst = L.featureGroup();
+let nonEnglishFirst = L.featureGroup();
 
-let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-	maxZoom: 16
-});
-
-Esri_WorldGrayCanvas.addTo(map)
-
-fetch(url)
-	.then(response => {
-		return response.json();
-		})
-    .then(data =>{
-                formatData(data)
-        }
-)
-
-let speakFluentEnglish = L.featureGroup();
-let speakOtherLanguage = L.featureGroup();
+let layers = {
+	"English as First Language <svg height='10' width='10'><circle cx='5' cy='5' r='4' stroke='black' stroke-width='1' fill='green' /></svg>": englishFirst,
+	"Non-English as First Language <svg height='10' width='10'><circle cx='5' cy='5' r='4' stroke='black' stroke-width='1' fill='red' /></svg>": nonEnglishFirst
+}
 
 // add variables for keeping track of the count for the charts
 let countEnglish = 0;
 let countNonEnglish = 0;
 
 
-let exampleOptions = {
+let circleOptions = {
     radius: 4,
     fillColor: "#ff7800",
     color: "#000",
     weight: 1,
     opacity: 1,
     fillOpacity: 0.8
-}
+};
+
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS2WyfKTyZJ-_ja3GGrxoAXwranavyDGXYsxeFUO4nvHpCJrkKhChymXQqUEyhdGLnz9VN6BJv5tOjp/pub?gid=1560504149&single=true&output=csv";
+
+const englishFirstLegendHTML = document.getElementById("englishFirstLegend");
+const nonEnglishFirstLegendHtml = document.getElementById("nonEnglishFirstLegend");
+
+const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
+
+
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+
+Esri_WorldGrayCanvas.addTo(map);
 
 function addMarker(data){
-    if(data.doyouspeakenglishfluently == "Yes"){
-        exampleOptions.fillColor = "green"
-        speakFluentEnglish.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak English fluently</h2>`))
-        createButtons(data.lat,data.lng,data.location)
-        // add to the running total
+    if(data['Is your English your first language?'] == "Yes"){
+        circleOptions.fillColor = "red"
+        englishFirst.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>English First Language</h2>`))
+        // add to the running total of English as first language speajers
         countEnglish += 1;
         }
     else{
-        exampleOptions.fillColor = "red"
-        speakOtherLanguage.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak other languages</h2>`))
-        createButtons(data.lat,data.lng,data.location)
+        circleOptions.fillColor = "blue"
+        nonEnglishFirst.addLayer(L.circleMarker([data.lat,data.lng],circleOptions).bindPopup(`<h2>Non-English First Language</h2>`))
         // add to the running total of non-English 
         countNonEnglish +=1;
     }
-    return data.timestamp
-}
+    return data
+};
 
-function createButtons(lat,lng,title){
-    const newButton = document.createElement("button");
-    newButton.id = "button"+title;
-    newButton.innerHTML = title;
-    newButton.setAttribute("lat",lat); 
-    newButton.setAttribute("lng",lng);
-    newButton.addEventListener('click', function(){
-        map.flyTo([lat,lng]);
+
+function loadData(url){
+    Papa.parse(url, {
+        header: true,
+        download: true,
+        complete: results => processData(results)
     })
-    // removed buttons
-    // const spaceForButtons = document.getElementById('contents')
-    // spaceForButtons.appendChild(newButton);
+};
+
+function processData(results){
+    console.log(results)
+    results.data.forEach(data => {
+        console.log(data)
+        addMarker(data)
+    })
+    englishFirst.addTo(map) 
+    nonEnglishFirst.addTo(map)  
+    let allLayers = L.featureGroup([englishFirst,nonEnglishFirst]);
+    map.fitBounds(allLayers.getBounds());
+    addChart() // call the addChart function after the data is added
+};
+
+loadData(dataUrl)
+
+englishFirstLegendHTML.addEventListener("click",toggleEnglishLayer) 
+
+function toggleEnglishLayer(){
+    if(map.hasLayer(englishFirst)){
+        map.removeLayer(englishFirst)
+    }
+    else{
+        map.addLayer(englishFirst)
+    }
 }
 
-function formatData(theData){
-        const formattedData = []
-        const rows = theData.feed.entry
-        for(const row of rows) {
-          const formattedRow = {}
-          for(const key in row) {
-            if(key.startsWith("gsx$")) {
-                  formattedRow[key.replace("gsx$", "")] = row[key].$t
-            }
-          }
-          formattedData.push(formattedRow)
-        }
-        console.log(formattedData)
-        formattedData.forEach(addMarker)
-        speakFluentEnglish.addTo(map)
-        console.log('speakFluentEnglish')
-        console.log(speakFluentEnglish)
-        speakOtherLanguage.addTo(map)
-        let allLayers = L.featureGroup([speakFluentEnglish,speakOtherLanguage]);
-        map.fitBounds(allLayers.getBounds());
-        addChart() // call the addChart function after the data is added
-}
+nonEnglishFirstLegendHtml.addEventListener("click",toggleNonEnglishLayer) 
 
-let layers = {
-	"Speaks English": speakFluentEnglish,
-	"Speaks Other Languages": speakOtherLanguage
+function toggleNonEnglishLayer(){
+    if(map.hasLayer(nonEnglishFirst)){
+        map.removeLayer(nonEnglishFirst)
+    }
+    else{
+        map.addLayer(nonEnglishFirst)
+    }
 }
 
 //function to add chart after the data gets created
@@ -103,7 +108,7 @@ function addChart(){
         type: 'pie', //can change to 'bar','line' chart or others
         data: {
             // labels for data here
-        labels: ["Speak English Fluently","Speaks Other Language"],
+        labels: ["English as First Language","Non-English as First Language"],
         datasets: [
             {
             label: "Count",
@@ -123,7 +128,3 @@ function addChart(){
         }
     });
 }
-
-
-
-L.control.layers(null,layers).addTo(map)
